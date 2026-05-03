d51296fe06eb55b5d7760e9515f381da5acc63ff
smbarki <sami.mbarki@gmail.com>
2026-05-03 02:16:24 +0100

    fix issues

--- 02-bu-services/95-learning-management/03-lms-service/src/main/java/eu/isygoit/security/config/JwtAuthFilter.java
+++ 02-bu-services/95-learning-management/03-lms-service/src/main/java/eu/isygoit/security/config/JwtAuthFilter.java
@@ -2,8 +2,8 @@ package eu.isygoit.security.config;
 
 import eu.isygoit.filter.jwt.JwtKmsClientAuthFilter;
 import eu.isygoit.jwt.IJwtService;
-import eu.isygoit.service.ITokenService;
 import eu.isygoit.service.RequestContextService;
+import eu.isygoit.service.TokenServiceApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthFilter extends JwtKmsClientAuthFilter {
 
-    public JwtAuthFilter(IJwtService jwtService, RequestContextService requestContextService, ITokenService tokenService) {
+    public JwtAuthFilter(IJwtService jwtService, RequestContextService requestContextService, TokenServiceApi tokenService) {
         super(jwtService, requestContextService, tokenService);
     }
 }
